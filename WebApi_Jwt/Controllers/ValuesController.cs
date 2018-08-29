37627f2dafed93ad0c7248634be669f3c827e1b0
FelixHuang <soupo.tw@gmail.com>
2018-08-29 13:15:31 +0800

    新增 Jwt Token 驗證與使用者資訊解析示範功能

--- WebApi_Jwt/Controllers/ValuesController.cs
+++ WebApi_Jwt/Controllers/ValuesController.cs
@@ -1,16 +1,21 @@
-﻿using System;
+﻿using Microsoft.IdentityModel.Tokens;
+using Microsoft.Owin.Security.DataHandler.Encoder;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Claims;
 using System.Web;
 using System.Web.Http;
+using WebApiJWT.Models;
 
 namespace WebApiJWT.Controllers
 {
     public class ValuesController : ApiController
     {
-        // GET api/values
-        [Authorize]
+        // GET api/values        
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
@@ -25,7 +30,7 @@ namespace WebApiJWT.Controllers
         }
 
         // GET api/values/getUserInfo
-        [HttpGet, ActionName("GetUserInfo")]
+        [HttpGet, Route("api/values/getUserInfo")]
         [Authorize]
         public object Get_UserInfo()
         {
@@ -54,12 +59,23 @@ namespace WebApiJWT.Controllers
         }
 
 
-
-        // GET api/values/5        
-        public string Get(int id)
+        /// <summary>
+        /// 手動驗證 JWT Token
+        /// </summary>
+        /// <param name="token">Token 字串</param>
+        /// <returns></returns>
+        //POST api/values/parseToken
+        [HttpPost, Route("api/values/parseToken")]
+        public IHttpActionResult Post_JwtTokenValidTest([FromBody]Form_Token model)
         {
-            return id.ToString();
+            object result = null;
+            if (!string.IsNullOrEmpty(model.Token))
+            {
+                result = TokenValidate(model.Token, model.ValidateLifetime);
+            }
+            return Ok(result);
         }
+
 
         // POST api/values
         public void Post([FromBody]string value)
@@ -89,6 +105,107 @@ namespace WebApiJWT.Controllers
             DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
             dtDateTime = dtDateTime.AddSeconds(unixTimeStamp).ToLocalTime();
             return dtDateTime;
+        }
+
+
+        public class Form_Token
+        {
+            /// <summary>
+            /// Jwt Token 字串
+            /// </summary>
+            public string Token { get; set; }
+
+            /// <summary>
+            /// 是否要驗證 Token 生命週期
+            /// </summary>
+            public bool ValidateLifetime { get; set; }
+        }
+
+
+        /// <summary>
+        /// 驗證 Jwt Token
+        /// </summary>
+        /// <param name="jwtInput">Token 內容</param>
+        /// <param name="validateLifetime">是否驗證生命週期</param>
+        /// <returns>解析後的 Token 內容或錯誤訊息</returns>
+        /// <remarks>.net core: https://dotblogs.com.tw/libtong/2018/01/11/121549</remarks>
+        public string TokenValidate(string jwtInput, bool validateLifetime = true)
+        {
+            string output = "";
+
+            try
+            {
+                var jwtHandler = new JwtSecurityTokenHandler();
+                var readableToken = jwtHandler.CanReadToken(jwtInput);
+
+                if (readableToken != true)
+                {
+                    output = "the token does not seem to be in a proper JWT format";
+                }
+                else
+                {
+                    var token = jwtHandler.ReadJwtToken(jwtInput);
+                    var headers = token.Header;
+                    var jwtHeader = "{";
+                    foreach (var h in headers)
+                    {
+                        jwtHeader += '"' + h.Key + "\":\"" + h.Value + "\",";
+                    }
+                    jwtHeader += "}";
+                    output = "Header:\r\n" + JToken.Parse(jwtHeader).ToString(Formatting.Indented);
+
+
+                    //Extract the payload of the JWT
+                    var claims = token.Claims;
+                    var jwtPayload = "{";
+                    foreach (Claim c in claims)
+                    {
+                        jwtPayload += '"' + c.Type + "\":\"" + c.Value + "\",";
+                    }
+                    jwtPayload += "}";
+                    output += "\r\nPayload:\r\n" + JToken.Parse(jwtPayload).ToString(Formatting.Indented);
+
+
+                    //Token 驗證
+                    SysConfig sysConfig = new SysConfig();
+                    Database _db = new Database();
+
+                    //至 DB 取出有效的 AudienceID 清單
+                    var audiences = _db.Audiences;
+                    string[] audience = audiences == null || audiences.Count() == 0 ? new string[0] : audiences.Select(x => x.AudienceId).ToArray();
+
+                    //加密secret
+                    string symmetricKeyAsBase64 = sysConfig.JwtBase64Secret;
+                    var keyByteArray = TextEncodings.Base64Url.Decode(symmetricKeyAsBase64);
+                    var securityKey = new SymmetricSecurityKey(keyByteArray);
+
+                    TokenValidationParameters paras = new TokenValidationParameters()
+                    {
+                        ValidateAudience = true,
+                        ValidAudiences = audience,
+                        ValidIssuer = sysConfig.JwtIssuer,
+                        IssuerSigningKey = securityKey,
+                        ValidateLifetime = validateLifetime,
+                    };
+
+                    ClaimsPrincipal _ValidateResult = jwtHandler.ValidateToken(jwtInput, paras, out SecurityToken validatedToken);
+
+                    //萃取使用者資訊
+                    var UserInfo = new
+                    {
+                        Account = _ValidateResult.FindFirst("Account").Value,
+                        Name = _ValidateResult.FindFirst("username").Value,
+                        RoleNames = _ValidateResult.Claims.Where(c => c.Type == ClaimTypes.Role).Select(c => c.Value).ToList(),
+                        TokenIssueDate = UnixTimeStampToDateTime(double.Parse(_ValidateResult.FindFirst("nbf").Value)).ToUniversalTime().AddHours(8),
+                        TokenExpiredDate = UnixTimeStampToDateTime(double.Parse(_ValidateResult.FindFirst("exp").Value)).ToUniversalTime().AddHours(8),
+                    };
+                }
+            }
+            catch (Exception ex)
+            {
+                output = ex.Message.ToString();
+            }
+            return output;
         }
     }
 }
