c561e319cf5b05903326d7ac6c5a056907a1eb82
FelixHuang <soupo.tw@gmail.com>
2019-01-17 15:45:04 +0800

    添加 WebAPI Versioning 示例

--- WebApi_Jwt/App_Start/WebApiConfig.cs
+++ WebApi_Jwt/App_Start/WebApiConfig.cs
@@ -1,7 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Microsoft.Web.Http;
+using Microsoft.Web.Http.Routing;
 using System.Web.Http;
+using System.Web.Http.Routing;
 
 namespace WebApiJwt
 {
@@ -10,9 +10,24 @@ namespace WebApiJwt
         public static void Register(HttpConfiguration config)
         {
             // Web API 設定和服務
+            var constraintResolver = new DefaultInlineConstraintResolver() { ConstraintMap = { ["apiVersion"] = typeof(ApiVersionRouteConstraint) } };
 
             // Web API 路由
-            config.MapHttpAttributeRoutes();
+            config.MapHttpAttributeRoutes(constraintResolver);
+
+            //API Versioning 設定
+            config.AddApiVersioning(o =>
+            {
+                //設 true，會將 API 支援的版本列在 header 中
+                o.ReportApiVersions = true;
+                //設定 API 預設的版本
+                o.DefaultApiVersion = new ApiVersion(2, 0);
+                //若使用時沒特別指定版本的，true: 會使用 DefaultApiVersion 設定的版本。
+                o.AssumeDefaultVersionWhenUnspecified = true;
+            });
+            //API Versioning 建議使用 Attribute Routing, 因此不另外設路由對映
+
+
 
             config.Routes.MapHttpRoute(
                 name: "ApiCustomRoute",
---
+++ WebApi_Jwt/Controllers/VersioningController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Web.Http;
+using System;
+using System.Web.Http;
+
+//API版本化範例
+//參考: https://dotblogs.com.tw/rainmaker/2017/03/12/130759
+
+// API                              v1.0    v2.0    v2019-01-16.1.0-Beta    Comment
+// ==============================   =====   =====   =====================   =============================
+// version-test-a/test              OK      OK      OK                      各版本有各自方法，回傳值皆不同
+// version-test-a/test_ex1          OK      OK      OK                      各版本繼承 v1
+// version-test-a/test_v2_ex1       -       OK      OK                      各版本繼承 v2
+// version-test-a/getDatetime       -       -       OK 
+
+
+//v1
+namespace WebApiJwt.Controllers
+{
+    //v1,v2,v2019-01-16.1.0-Beta 皆可執行 VersioningExample1Controller 包含的所有 API
+    [ApiVersion("1.0")]                 //初始版本
+    [ApiVersion("2.0")]                 //使 v2.0 可繼承執行 v1 的版本
+    [ApiVersion("2019-01-16.1.0-Beta")] //使 v2019-01-16.1.0-Beta 可繼承執行 v1 的版本
+    [RoutePrefix("api/v{version:apiVersion}/version-test-a")]
+    public class VersioningExample1Controller : ApiController
+    {
+        //v1 原始版本 (後續各版號執行各自版本的 API)
+        //GET api/v1/version-test-a/test
+        [Route("test")]
+        public string Get() => "Hello world v1!";
+
+
+        //適用於 v1, v2, v2019-01-16.1.0-Beta，皆執行同一方法
+        //GET api/v1/version-test-a/test_ex1
+        [Route("test_ex1")]
+        public IHttpActionResult Get_Test()
+        {
+            return Ok($"Hello test_ex1. This API supports from v1.");
+        }
+    }
+}
+
+
+//v2: 此版本提供新 API: test_v2_ex1
+namespace WebApiJwt.Controllers_v2
+{
+    //獨有功能，僅有本版本可執行 
+    [ApiVersion("2.0")]
+    [ApiVersion("2019-01-16.1.0-Beta")] //使 v2019-01-16.1.0-Beta 可繼承執行 v2 的版本
+    [RoutePrefix("api/v{version:apiVersion}/version-test-a")]
+    public class VersioningExample1Controller : ApiController
+    {
+        //v2 改版後功能，各版號執行各自版本的 API
+        //GET: api/v2/version-test-a/test
+        [Route("test")]
+        [MapToApiVersion("2.0")]
+        public string Get() => "Hello world v2!";
+
+        //v2 獨有的新功能
+        //GET: api/v2/version-test-a/test_v2_ex1
+        [Route("test_v2_ex1")]        
+        public string Get_V2Above() => "Hello world, test_v2_ex1. This API supports from v2.";
+    }
+}
+
+
+//v3 (2019-01-16.1.0-Beta): 本版本提供新 API: getDatetime
+namespace WebApiJwt.Controllers_v2019_01_16_1_0_Beta
+{
+    //獨有功能，僅有本版本可執行     
+    [ApiVersion("2019-01-16.1.0-Beta")]
+    [RoutePrefix("api/v{version:apiVersion}/version-test-a")]
+    public class VersioningExample1Controller : ApiController
+    {
+        //v3 改版後功能，各版號執行各自版本的 API
+        //GET: api/v2019-01-16.1.0-Beta/version-test-a/test
+        [Route("test")]
+        [MapToApiVersion("2019-01-16.1.0-Beta")]
+        public string Get() => "Hello world v2019-01-16.1.0-Beta!";
+
+        //v3 獨有功能
+        //GET: api/v2019-01-16.1.0-Beta/version-test-a/getDatetime
+        [Route("getDatetime")]
+        public string Get_DateTime() => $"Current time: {DateTime.Now.ToShortDateString()} {DateTime.Now.ToShortTimeString()}";
+    }
+}
